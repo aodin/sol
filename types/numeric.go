98e464e3a76ee6c7a87e292efa62ecdcc2af7b3a
Aaron O. Ellis <aaronoellis@gmail.com>
2016-05-31 12:11:02 -0600

    More numeric types

--- types/numeric.go
+++ types/numeric.go
@@ -2,12 +2,31 @@ package types
 
 type numeric struct {
 	base
-	min, max int64
+	min, max         int64
+	precision, scale int
 }
+
+var _ Type = numeric{}
 
 func (t numeric) NotNull() numeric {
 	t.base.NotNull()
 	return t
+}
+
+func Decimal(precision, scale int) numeric {
+	typ := Numeric(precision, scale)
+	typ.base.name = "DECIMAL"
+	return typ
+}
+
+func Integer() numeric {
+	return numeric{
+		base: base{
+			name: "INTEGER",
+		},
+		min: -2147483648,
+		max: 2147483647,
+	}
 }
 
 func SmallInt() numeric {
@@ -20,12 +39,46 @@ func SmallInt() numeric {
 	}
 }
 
-func Integer() numeric {
+func BigInt() numeric {
 	return numeric{
 		base: base{
-			name: "INTEGER",
+			name: "BIGINT",
 		},
-		min: -2147483648,
-		max: 2147483647,
+		min: -9223372036854775808,
+		max: 9223372036854775807,
+	}
+}
+
+func Numeric(precision, scale int) numeric {
+	return numeric{
+		base: base{
+			name: "NUMERIC",
+		},
+		precision: precision,
+		scale:     scale,
+	}
+}
+
+func Float() numeric {
+	return numeric{
+		base: base{
+			name: "FLOAT",
+		},
+	}
+}
+
+func Real() numeric {
+	return numeric{
+		base: base{
+			name: "REAL",
+		},
+	}
+}
+
+func Double() numeric {
+	return numeric{
+		base: base{
+			name: "DOUBLE",
+		},
 	}
 }
---
+++ types/numeric_test.go
@@ -0,0 +1,16 @@
+package types
+
+import (
+	"testing"
+)
+
+func TestNumeric(t *testing.T) {
+	typ := Double()
+	create, err := typ.Create(nil)
+	if err != nil {
+		t.Errorf("Unexpected error during DOUBLE Create(): %s", err)
+	}
+	if create != "DOUBLE" {
+		t.Errorf("Unexpected output of DOUBLE type: %s", create)
+	}
+}
